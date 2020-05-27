253b9d8caa7dc3e8a4edc9fe6899211099d96943
Jason Morsley <GitHub@jasonmorsley.dev>
2020-05-27 18:41:15 +0100

    Terraform Registry Compatible Module

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-terraform-aws-keys
+﻿# Terraform Key Pairs in AWS
+
+## Examples
+
+For examples how to use, please refer to the examples folder.
+
+## Usage
+
+```
+module "keys" {
+
+  source = "jason-morsley/aws-keys"
+
+  name = "example"
+
+  tags = {
+    Terraform = "true"
+  }
+
+}
+```
---
+++ outputs.tf
@@ -0,0 +1,24 @@
+﻿#       ____        _               _       
+#      / __ \      | |             | |      
+#     | |  | |_   _| |_ _ __  _   _| |_ ___
+#     | |  | | | | | __| '_ \| | | | __/ __|
+#     | |__| | |_| | |_| |_) | |_| | |_\__ \
+#      \____/ \__,_|\__| .__/ \__,_|\__|___/
+#                      | |                  
+#                      |_|
+
+output "key_pair" {
+  value = aws_key_pair.this
+}
+
+output "key_name" {
+  value = aws_key_pair.this.key_name
+}
+
+output "public_key" {
+  value = tls_private_key.this.public_key_openssh
+}
+
+output "private_key" {
+  value = tls_private_key.this.private_key_pem
+}
